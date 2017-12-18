d0e3aeb3e4b1d5a5d681eb3767eef24918eebc68
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-18 22:41:18 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,4 +1,6 @@
 class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:show, :edit, :update]
+  
   def index
   end
 
@@ -9,5 +11,15 @@ class IngredientsController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :recipe)
+  end
+
+  def set_recipe
+    @ingredient = Ingredient.find(params[:id])
   end
 end
