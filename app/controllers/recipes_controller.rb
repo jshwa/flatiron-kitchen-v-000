720a533201a786e5722c7a601e2c121a6c506baa
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-18 22:33:47 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,8 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only [:show, :edit, :update]
+
   def index
+    @recipes = Recipe.all
   end
 
   def show
@@ -9,5 +12,15 @@ class RecipesController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :ingredient)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
